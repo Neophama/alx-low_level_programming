4f0fe3c8bcd882738fad6e65948166407fa87987
Neophama <neophama@gmail.com>
2023-02-17 16:41:22 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z'){
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z'){
+			putchar(d);
+			d++;
+		}
+	purchar('\n');
+	return(0);
+}
